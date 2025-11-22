9ec98773b0eb4efe8f184d72f46544253b7cf5a1
TheFolkor <folkorthe@gmail.com>
2025-11-22 14:08:10 -0600

    BP

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,21 @@
-import { world } from "@minecraft/server";
+import {
+    system,
+    CustomComponentParameters,
+    ItemComponentBeforeDurabilityDamageEvent,
+    ItemComponentCompleteUseEvent,
+    ItemComponentConsumeEvent,
+    ItemComponentHitEntityEvent,
+    ItemComponentMineBlockEvent,
+    ItemComponentUseEvent,
+    ItemComponentUseOnEvent,
+} from "@minecraft/server";
 import { shootCommon, shootRepeat } from "./globalVar/u.js";
 
-// Escuchar cuando un jugador usa cualquier ítem
-world.afterEvents.itemUse.subscribe(ev => {
-
-    const { itemStack, source } = ev;
-
-    if (!itemStack || !source) return;
-
-    // Revisar si el ítem tiene el componente "udaw:weapon"
-    const comp = itemStack.getComponent("minecraft:custom_components");
-
-    if (!comp || !comp.has("udaw:weapon")) return;
-
-    // Si no está agachado → disparar
-    if (!source.isSneaking) {
-        shootCommon(source, "minecraft:arrow", 1, 1);
-    }
+system.beforeEvents.startup.subscribe((startupEvent) => {
+    startupEvent.itemComponentRegistry.registerCustomComponent("udaw:weapon", {
+        onUse(event) {
+            const { itemStack, source } = event;
+            shootCommon(source, "minecraft:arrow", 1, 1);
+        },
+    });
 });
