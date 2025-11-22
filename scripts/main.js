80cc901fb494bf62ff3c5f92187bb1f854008c7f
TheFolkor <folkorthe@gmail.com>
2025-11-22 13:43:42 -0600

    BP

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,16 @@
-import { system } from "@minecraft/server";
+import {
+    system,
+} from "@minecraft/server";
 import { VECTOR } from "./mathUtils";
+import { shootCommon, shootRepeat } from "./u.js";
+
+system.beforeEvents.startup.subscribe((udaw) => {
+    udaw.itemComponentRegistry.registerCustomComponent("udaw:weapon", {
+        onUse(ev) {
+            const { itemStack, source } = ev;
+            if (!source.isSneaking) {
+                shootCommon(source, "minecraft:arrow", 1, 1);
+            }
+        } 
+    })
+});
