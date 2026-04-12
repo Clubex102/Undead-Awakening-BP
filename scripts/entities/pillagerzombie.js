7ed1bd6896774e2010a9a2ccef3868b00a2ebea7
nickbeans890 <nickthehedgehog3@gmail.com>
2026-04-12 15:29:02 -0600

    aa

--- scripts/entities/pillagerzombie.js
+++ scripts/entities/pillagerzombie.js
@@ -79,7 +79,7 @@ function applyKnockdown(victim) {
     } catch (_) {}
 
     try {
-        let tackleAnim = KNOCKDOWN_ANIM; // por defecto para jugadores
+        let tackleAnim = KNOCKDOWN_ANIM; // por defecto para players
         
         if (victim.typeId === "minecraft:iron_golem") {
             tackleAnim = "animation.irongolem.tackled";
