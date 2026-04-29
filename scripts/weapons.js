642de00b73259384e670fc1f9b15687e4fb5a744
Clubex <149812994+Clubex102@users.noreply.github.com>
2026-04-29 11:55:18 -0600

    fds

--- scripts/weapons.js
+++ scripts/weapons.js
@@ -227,8 +227,6 @@ system.beforeEvents.startup.subscribe((startupEvent) => {
             const loaded = convertItem(item, "udaw:flintlockgun_loaded");
             mainhand.setItem(loaded);
             source.dimension.playSound("crossbow.loading.end", source.location);
-            
-
             const id = source.id;
             loadedPlayers[id] = true;
             system.runTimeout(() => { delete loadedPlayers[id]; }, 7);
@@ -244,7 +242,7 @@ system.beforeEvents.startup.subscribe((startupEvent) => {
             if (!hasItem(source, AMMO_ITEM)) {
                 mainhand.setItem(convertItem(item, "udaw:flintlockgun_unusable"));
             } else {
-                source.dimension.playSound("reload2", source.location);
+                source.dimension.playSound("crossbow.loading.start", source.location);
                 system.runTimeout(() => {
                     source.dimension.playSound("crossbow.loading.middle", source.location);
                 }, 10);
